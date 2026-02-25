b46cd591ed0635ce56c02d17733fc115ae6410dd
Avii520 <avijitcb20@gmail.com>
2026-02-25 20:34:38 +0600

    Impliment the first part of the hero section

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // ১. এই লাইনটি নতুন যোগ করবেন
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // ২. এখানে কমা দিয়ে tailwindcss() যোগ করবেন
-  ],
+  plugins: [react(), tailwindcss()],
 });
